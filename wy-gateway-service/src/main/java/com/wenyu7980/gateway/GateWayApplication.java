d572922d84b230269c879dcb14246090b373f7f8
wenyu <wenyu7980@163.com>
2020-11-25 21:47:55 +0800

    feign config

--- wy-gateway-service/src/main/java/com/wenyu7980/gateway/GateWayApplication.java
+++ wy-gateway-service/src/main/java/com/wenyu7980/gateway/GateWayApplication.java
@@ -1,6 +1,6 @@
 package com.wenyu7980.gateway;
 
-import com.wenyu7980.common.feign.config.FeignConfig;
+import com.wenyu7980.common.feign.config.FeignClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(value = "com.wenyu7980", defaultConfiguration = FeignConfig.class)
+@EnableFeignClients(value = "com.wenyu7980", defaultConfiguration = FeignClientConfig.class)
 public class GateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
